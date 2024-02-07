1cad88cd9dff2bf9655a19207cddd1dae8b3871b
Daniel Marín López <99746344+Dansarasix-DML@users.noreply.github.com>
2024-02-07 09:04:10 +0100

    Delete Ud6 directory

--- Ud6/ejemplos/veterinaria/app/Config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    define("DBHOST", "localhost");
-    define("DBUSER", "veterinaria");
-    define("DBPASS", "root");
-    define("DBNAME", "veterinaria");
-    define("DBPORT", 3306);
-?>
